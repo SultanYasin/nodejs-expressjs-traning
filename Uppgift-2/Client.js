1a8ee8fbc059ed79e47bf35c232eb053b8ec817d
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2022-09-13 18:04:19 +0200

    making a chatapp server

--- Uppgift-2/Client.js
+++ Uppgift-2/Client.js
@@ -1,7 +1,12 @@
 const net = require('node:net')
+const readline = repuire('readLine');
 
-const clientSite = net.createConnection('8090', 'localhost')
- clientSite.on('data', (data , error) => {
-    if(error) console.error(error)
-    console.log(`\n message received from the server:  ${data}`)
- });
+readline.createInterface({input : process.stdin, output : process.stdout})
+
+const username = 
+
+
+ /* 
+ 1- way to take input from user -> readline.createInterface({input: process.stdin , output: procsess.stdout})
+ 2- force the user to enter a name otherwise he can not connect to the server 
+  */
--- Uppgift-2/chatServer.js
+++ Uppgift-2/chatServer.js
@@ -1,6 +1,14 @@
 const net = require('node:net')
 
-const server = net.createServer()
+var users = [];
+
+const server = net.createServer(socket =>{    
+    users.push(socket) 
+    console.log('Client connected to the server!');
+    socket.on('data' , data =>broadcast(data , socket) ) 
+    socket.on('error' , error => console.error('A client has disconnected')) 
+    socket.on('close' , ()=> console.log('A client has left the chat.'))
+})
 
 server.listen({
     host : 'localhost',
@@ -8,7 +16,83 @@ server.listen({
     exclusive : true
 })
 
+function broadcast(message , messageSender) {
+    const senderSocket = users.indexOf(messageSender)
+    (message.toString() === 'quit') ? users.splice(senderSocket , 1) 
+    : users.forEach(user => { if(user !== messageSender) user.write(message)})  
+}
+
+
+
+/* 
+  users.push(socket) // adding users to the array
+
+    handling the messages that been send from the user and boadcasted it.
+    this function except the message that user send and a socket to know who send the message.
+    socket.on('data' , data =>broadcast(data , socket) ) 
+
+     logging when a user disconnect from the server. 
+    socket.on('error' , error => console.error('A client has disconnected')) 
+
+    logging when a user leave the chat server.
+    socket.on('close' , ()=> console.log('A client has left the chat.'))
+*/
+
+
+/* 
+function broadcast(message , messageSender)
+this function takes the message (as a param)to send it to every one in the group chat,
+ and messageSender(as a param) so that the sender don't receive the message he send him self.
+
+ then I check if the message that messageSender is 'quit' so that mean they msut be removed from the users array by using 
+ users.indexOf(messageSender) 
+ -> const senderSocket = users.indexOf(messageSender) then splice methode that remove Items from arrays
+ -> (message.toString() === 'quit') ? users.splice(senderSocket , 1) 
+
+ if message is not 'quit' so I wanna to broadcast to all users in the array by looping throgh the array of useres, except the one who sende the message.
+ -> users.forEach(user => {if(user !== messageSender) user.werite(message)})
+
+ and if the message is not quit
+*/
+
+
+
+
+
+
+
 server.on('connection' , (client) => {
     console.log('Client connected')
     client.write('welcome to the server... ')
 })
+
+
+
+
+
+
+//tcp transition controll protocol
+
+
+/* 
+create server createServer(socket=>{})
+make aray of users
+socket.on('data' , data=>{})
+socket.on('error' , error=>{})
+socket.on('close' , ()=>)
+__
+listen to a specific portnumber
+__
+
+
+server = net.createServer() listens for incomming connections -> when it receive one it creates a net.socket object to communicate with whoever connected
+*Server -> event emitter is a moduel that facilitata communication between nodejs objects
+array contained sockets(socket is an end point connection between 2 program) users in other word. this arrays work is to send the message to all users in this array 
+________
+must make the server listen to the messages through socket.on(){the one that should be passed in createServer(socket)}
+-> socket.on('data' , (data)=> {}) data is the messages that sendes from the client then use broadcast methode to send the messages to the users
+
+__________
+broadcast function must take 2 param a- the sent message , b- the user that sent this message through his socket number
+
+*/
